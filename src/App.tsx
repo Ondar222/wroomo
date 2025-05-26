59fdac103bc6d1431abc86349b94d61d3863902b
Ondar222 <Arslanondar2003@gmail.com>
2025-05-26 16:33:01 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import MainLayout from './components/layout/MainLayout';
-import Hero from './components/home/Hero';
-import PopularVehicles from './components/home/PopularVehicles';
-import LocationSection from './components/home/LocationSection';
-import HowItWorks from './components/home/HowItWorks';
-import Testimonials from './components/home/Testimonials';
-import AppDownload from './components/home/AppDownload';
+import React from "react";
+import MainLayout from "./components/layout/MainLayout";
+import Hero from "./components/home/Hero";
+import PopularVehicles from "./components/home/PopularVehicles";
+import LocationSection from "./components/home/LocationSection";
+import HowItWorks from "./components/home/HowItWorks";
+import Testimonials from "./components/home/Testimonials";
+import AppDownload from "./components/home/AppDownload";
 
 function App() {
   return (
--- src/components/pages/VehicleDetailPage.tsx
+++ src/components/pages/VehicleDetailPage.tsx
@@ -1,16 +1,12 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-import { Star, MapPin, Users, BarChart3, Car, Bike } from "lucide-react";
+import React, { useState } from "react";
 import { motorcycles, cars } from "../../data/vehicles";
-import { VehicleType, MotorcycleType, CarType } from "../../types/vehicle";
-import "../../styles/VehicleDetails.css"; // We'll create this CSS file
+import { VehicleType } from "../../types/vehicle";
+import "../../styles/VehicleDetails.css";
 
 const VehicleDetailPage = () => {
-  const { id } = useParams<{ id: string }>();
-
-  // Combine motorcycles and cars arrays
+  const [vehicleId] = useState(window.location.pathname.split("/").pop());
   const allVehicles = [...motorcycles, ...cars];
-  const vehicle = allVehicles.find((v) => v.id === id);
+  const vehicle = allVehicles.find((v) => v.id === vehicleId);
 
   if (!vehicle) {
     return (
@@ -23,13 +19,17 @@ const VehicleDetailPage = () => {
   const renderSpecificationIcon = (icon: string) => {
     switch (icon) {
       case "users":
-        return <Users size={16} className="spec-icon" />;
+        return (
+          <svg className="spec-icon" width="16" height="16" viewBox="0 0 24 24">
+            <path d="M12 4a4 4 0 0 1 4 4 4 4 0 0 1-4 4 4 4 0 0 1-4-4 4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4z" />
+          </svg>
+        );
       case "engine":
-        return <BarChart3 size={16} className="spec-icon" />;
-      case "gear":
-      // return <Gear size={16} className="spec-icon" />;
-      case "door":
-      // return <Door size={16} className="spec-icon" />;
+        return (
+          <svg className="spec-icon" width="16" height="16" viewBox="0 0 24 24">
+            <path d="M7 4v2h3v2H7l-2 2v3H3v-3H1v8h2v-3h2v3h3l2 2h8v-4h2v3h3V9h-3v3h-2V8h-6V6h3V4H7z" />
+          </svg>
+        );
       default:
         return null;
     }
@@ -77,14 +77,35 @@ const VehicleDetailPage = () => {
             <div className="vehicle-type-rating">
               <div className="vehicle-type-tag">
                 {vehicle.vehicleType === "motorcycle" ? (
-                  <Bike size={20} className="vehicle-type-icon" />
+                  <svg
+                    className="vehicle-type-icon"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M17.5 10.5h2v1h-2v-1zm-13 0h2v3h-2v-3zm15 3h1.5v-3H22v3h-2.5zM18 10c-.8 0-1.4.4-1.8 1H12l-1.9-3.3c-.3-.5-1-.7-1.5-.4-.5.3-.7 1-.4 1.5L10 11H5.8c-.5-1.2-1.6-2-2.8-2-1.7 0-3 1.3-3 3s1.3 3 3 3c1.2 0 2.3-.8 2.8-2h8.4c.5 1.2 1.6 2 2.8 2 1.7 0 3-1.3 3-3s-1.3-3-3-3zM5 13.5c-.8 0-1.5-.7-1.5-1.5s.7-1.5 1.5-1.5 1.5.7 1.5 1.5-.7 1.5-1.5 1.5zm13 0c-.8 0-1.5-.7-1.5-1.5s.7-1.5 1.5-1.5 1.5.7 1.5 1.5-.7 1.5-1.5 1.5z" />
+                  </svg>
                 ) : (
-                  <Car size={20} className="vehicle-type-icon" />
+                  <svg
+                    className="vehicle-type-icon"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M18.9 6c-.4-1.2-1.5-2-2.8-2H7.9c-1.3 0-2.4.8-2.8 2H3v3h1.1c-.1.5-.1 1 0 1.5H3v3h1.1c.4 1.2 1.5 2 2.8 2h8.2c1.3 0 2.4-.8 2.8-2H21v-3h-1.1c.1-.5.1-1 0-1.5H21V6h-2.1zM7.9 14c-.8 0-1.5-.7-1.5-1.5S7.1 11 7.9 11s1.5.7 1.5 1.5S8.7 14 7.9 14zm8.2 0c-.8 0-1.5-.7-1.5-1.5s.7-1.5 1.5-1.5 1.5.7 1.5 1.5-.7 1.5-1.5 1.5z" />
+                  </svg>
                 )}
                 <span className="type-badge">{vehicle.type}</span>
               </div>
               <div className="rating-container">
-                <Star size={16} className="star-icon" />
+                <svg
+                  className="star-icon"
+                  width="16"
+                  height="16"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+                </svg>
                 <span className="rating-value">{vehicle.rating}</span>
                 <span className="review-count">({vehicle.reviewCount})</span>
               </div>
@@ -98,7 +119,14 @@ const VehicleDetailPage = () => {
               </h2>
 
               <div className="location">
-                <MapPin size={14} className="location-icon" />
+                <svg
+                  className="location-icon"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
+                </svg>
                 <span>{vehicle.location}</span>
               </div>
 
