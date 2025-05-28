ec40358d6fe24ede0f3d364010d5c048465a4c04
Ondar222 <Arslanondar2003@gmail.com>
2025-05-28 16:10:15 +0700

    commits

--- src/main.tsx
+++ src/main.tsx
@@ -3,21 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./app/router";
-
 import MainLayout from "./components/layout/MainLayout";
 
-// Убедитесь, что элемент существует перед рендерингом
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <MainLayout>
-    <React.StrictMode>
-      <BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter>
+      <MainLayout>
         <Router />
-      </BrowserRouter>
-    </React.StrictMode>
-  </MainLayout>
+      </MainLayout>
+    </BrowserRouter>
+  </React.StrictMode>
 );
